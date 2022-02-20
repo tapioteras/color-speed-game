ee0be00a8b8c00ec6f4c916658bc6f41a1272e7f
Tapio Teras <tapio.teras@reaktor.com>
2022-02-20 15:23:10 +0200

    Apply logic for blinking buttons

--- src/App.tsx
+++ src/App.tsx
@@ -14,25 +14,37 @@ interface ButtonProps {
   key: number
 }
 
-const Button: React.FC<ButtonProps> = ({color, onClick, isActive, key}) =>
-  <Box key={`${key}-button`} {...{onClick}}>
+const Button: React.FC<ButtonProps> = ({color, onClick, isActive, key}) => {
+  return <Box key={`${key}-button`} {...{onClick}}>
     <svg width="200px" height="200px">
-      <circle id="what" cx="100" cy="100" r="100" fill={color} />
+      <circle id="what" cx="100" cy="100" r="100" fill={isActive ? "gray" : color}/>
     </svg>
   </Box>
+}
 
-const getRandomMember = (arraySource: String[]): String => arraySource[Math.floor(Math.random() * arraySource.length)]
+const getRandomMember = (arraySource: any[]) => arraySource[Math.floor(Math.random() * arraySource.length)]
 
 const generateGame = (amountOfButtons = 4, startLevel = 1) => {
+  const [activeButton, setActiveButton] = React.useState<AvailableColor>()
+  const [buttons] = React.useState(Array.from(Array(amountOfButtons).keys()))
+  React.useEffect(() => {
+    const interval = setInterval(() => {
+      const nextActiveButton = Object.values(available_colors)[getRandomMember(buttons)] as AvailableColor
+      setActiveButton(nextActiveButton)
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
   return <Flex>
-    {Array.from(Array(amountOfButtons).keys()).map(i =>
-      <Box>
-      <Button
-        key={i}
-        color={Object.values(available_colors)[i] as AvailableColor}
-        onClick={() => alert("hei")}
-        isActive={true} />
-      </Box>
+    {buttons.map(i => {
+      const [currentButtonColor] = React.useState<AvailableColor>(Object.values(available_colors)[i] as AvailableColor)
+        return <Box>
+          <Button
+            key={i}
+            color={currentButtonColor}
+            onClick={() => alert("hei")}
+            isActive={currentButtonColor == activeButton}/>
+        </Box>
+      }
     )}
   </Flex>
 }
